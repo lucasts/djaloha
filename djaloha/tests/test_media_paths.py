c2a1e71763ec62639a36f41788e00a35fa1bdcf1
Enrico <rico.bl@gmail.com>
2012-10-04 15:03:43 -0300

    Added test for the require.js inclusion on newer Aloha versions.

--- djaloha/tests/test_media_paths.py
+++ djaloha/tests/test_media_paths.py
@@ -4,23 +4,18 @@ import re
 from djaloha.widgets import AlohaInput
 from django.conf import settings
 
+REQUIRE_JS_MATCH = re.compile(r'src="/static/(.*)/lib/require\.js"')
 ALOHA_JS_MATCH = re.compile(r'src="/static/(.*)/lib/aloha\.js"')
 ALOHA_CSS_MATCH = re.compile(r'href="/static/(.*)/css/aloha\.css"')
 
 
-class MediaPathTestCase(TestCase):
+class WidgetMediaTestCase(TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.widget = AlohaInput()
 
-    def tearDown(self):
-        try:
-            del settings.DJALOHA_ALOHA_VERSION
-        except AttributeError:
-            pass
-
-    def _assertHasMediaForVersion(self, content, version, regex, message):
+    def assertHasMediaForVersion(self, content, version, regex, message):
         for tags in content:
             matched_src = regex.search(tags)
             if matched_src:
@@ -30,37 +25,45 @@ class MediaPathTestCase(TestCase):
         self.fail(message)
 
     def assertHasCssForVersion(self, content, version, message):
-        self._assertHasMediaForVersion(content, version, ALOHA_CSS_MATCH,
-                                       message)
+        self.assertHasMediaForVersion(content, version, ALOHA_CSS_MATCH,
+                                      message)
 
     def assertHasJsForVersion(self, content, version, message):
-        self._assertHasMediaForVersion(content, version, ALOHA_JS_MATCH,
-                                       message)
+        self.assertHasMediaForVersion(content, version, ALOHA_JS_MATCH,
+                                      message)
 
-    def test_aloha_js_path_must_include_aloha_version_number_from_settings(self):
+    def test_should_include_js_for_custom_version(self):
         aloha_version = 'aloha.0.22.1'
         with self.settings(DJALOHA_ALOHA_VERSION=aloha_version):
             content = self.widget.media.render_js()
             self.assertHasJsForVersion(
                 content, aloha_version,
-                "A path to the Aloha javascript file wasn't found.")
+                "The Aloha javascript file wasn't found.")
 
-    def test_aloha_css_path_must_include_aloha_version_number_from_settings(self):
+    def test_should_include_css_for_custom_version(self):
         aloha_version = 'aloha.0.22.1'
         with self.settings(DJALOHA_ALOHA_VERSION=aloha_version):
             content = self.widget.media.render_css()
             self.assertHasCssForVersion(
                 content, aloha_version,
-                "A path to the Aloha css file wasn't found.")
+                "The Aloha css file wasn't found.")
 
-    def test_default_css_path_must_be_use_aloha_0_20_20_version(self):
+    def test_should_include_require_js_for_newer_versions(self):
+        aloha_version = 'aloha.0.22.1'
+        with self.settings(DJALOHA_ALOHA_VERSION=aloha_version):
+            content = self.widget.media.render_js()
+            self.assertHasMediaForVersion(content, aloha_version,
+                                          REQUIRE_JS_MATCH,
+                                          "require.js wasn't found.")
+
+    def test_should_include_default_version_js(self):
         aloha_version = 'aloha.0.20.20'
         content = self.widget.media.render_css()
         self.assertHasCssForVersion(
             content, aloha_version,
             "A path to the Aloha css file wasn't found.")
 
-    def test_default_js_path_must_be_use_aloha_0_20_20_version(self):
+    def test_should_include_default_version_css(self):
         aloha_version = 'aloha.0.20.20'
         content = self.widget.media.render_js()
         self.assertHasJsForVersion(
